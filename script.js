21b6060a0713de09e62009a4c8eb41aaf77ceaa6
Kabir Dugal <kdugal747@gmail.com>
2021-02-12 14:55:37 -0500

    updated UI, new gameplay, fixed 5th click bug

--- script.js
+++ script.js
@@ -1,110 +1,104 @@
-// Function that randomly returns one of Rock, Paper or Scissors  
-function computerPlay() {
-    let gameOptions = ['rock', 'paper', 'scissors']
-    return gameOptions[Math.floor(Math.random() * gameOptions.length)];
-}
+let playerScore = 0;
+let computerScore = 0;
+let gameScore = String(playerScore) + " to " + String(computerScore);
 
-// Function that plays a single round of RPS
-function playRound(playerSelection, computerSelection) {
-    // case-insensitive
-    playerSelection = playerSelection.toLowerCase();
+const playerScoreBox = document.getElementById("your-score-box");
+const computerScoreBox = document.getElementById("computer-score-box");
+const resultsOutputBox = document.querySelector(".results-output-box");
+const optionButtons = document.querySelectorAll(".option-button");
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const resultsContainer = document.querySelector('.results-container')
 
-    if (playerSelection == 'rock') {
-        if (computerSelection == 'scissors') {
-            return "You Win! Rock beats Scissors";
-        } else if (computerSelection == 'paper') {
-            return "You Lose! Paper beats Rock";
-        } else {
-            return 'You Tied!';
+const newGame = document.createElement('button');
+newGame.textContent = `Play again`;
+
+// Function that plays a game of RPS
+function playGame(playerSelection, computerSelection) {
+  playerSelection = this.id;
+  computerSelection = computerPlay();
+
+
+    if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" &&
+        computerSelection === "paper" &&
+        (computerScore <= 5 || playerScore <= 5))
+    ) {
+      playerScore++;
+      playerScoreBox.textContent = `${playerScore}`;
+      resultsOutputBox.textContent =
+        "You Won! " +
+        capitalize(`${playerSelection}`) +
+        " beats " +
+        capitalize(`${computerSelection}`);
+        if (playerScore >= 5) {
+            optionButtons.forEach((btn) => btn.removeEventListener("click", playGame));
+            optionButtons.forEach((btn) => btn.classList.remove('option-button'));
+            optionButtons.forEach((btn) => btn.classList.add('option-button-end-game'));
+            resultsOutputBox.textContent = 'You Won! WOOHOOO'
+            playNewGame();
         }
-    } else if (playerSelection == 'paper') {
-        if (computerSelection == 'rock') {
-            return 'You Win! Paper beats Rock';
-        } else if (computerSelection == 'scissors') {
-            return 'You Lose! Scissors beats Paper';
-        } else {
-            return 'You Tied!';
+    } else if (
+      (playerSelection === "rock" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" &&
+        computerSelection === "rock" &&
+        (computerScore <= 5 || playerScore <= 5))
+    ) {
+      computerScore++;
+      computerScoreBox.textContent = `${computerScore}`;
+      resultsOutputBox.textContent =
+        "You Lost! " +
+        capitalize(`${computerSelection}`) +
+        " beats " +
+        capitalize(`${playerSelection}`);
+        if (computerScore >= 5) {
+            optionButtons.forEach((btn) => btn.removeEventListener("click", playGame));
+            optionButtons.forEach((btn) => btn.classList.remove('option-button'));
+            optionButtons.forEach((btn) => btn.classList.add('option-button-end-game'));
+            resultsOutputBox.textContent = 'You Lost! Try Again'
+            playNewGame();
         }
-    } else { // player selects scissors
-        if (computerSelection == 'paper') {
-            return 'You Win! Scissors beats Paper';
-        } else if (computerSelection == 'rock') {
-            return 'You Lose! Rock beats Scissors';
-        } else {
-            return 'You Tied!';
-        }
-    }
-}
-
-
-
-// Function that plays a 5-round RPS game that keeps score and reports the winner and loser of the game
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (i = 0; i < 5; i++) {
-        let playerSelection = prompt('Enter Rock, Paper, or Scissors:');
-        let computerSelection = computerPlay();
-        let roundResult = 'Round ' + (i + 1) + ' Results: ' + playRound(playerSelection, computerSelection);
-
-        //console.log(roundResult);
-
-        if (roundResult.indexOf('You Win') > 0 && roundResult.indexOf('You Lose') < 0) {
-            playerScore++;
-        } else if (roundResult.indexOf('You Win') < 0 && roundResult.indexOf('You Lose') > 0) {
-            computerScore++;
-        } else {
-            // pass
-        }
-    }
-
-    if (playerScore > computerScore) {
-        //console.log('Congrats! You Win. Final Score - You: ' + playerScore + ', Computer: ' + computerScore);
-    } else if (computerScore > playerScore) {
-        //console.log('You Lose! Final Score - You: ' + playerScore + ', Computer: ' + computerScore);
     } else {
-        //console.log('You Tied! Final Score - You: ' + playerScore + ', Computer: ' + computerScore)
+      resultsOutputBox.textContent = "You Tied!";
     }
 }
 
-let computerSelection = computerPlay();
-const resultsOutput = document.querySelector('.results-output-box')
+// Function that randomly returns one of Rock, Paper or Scissors
+function computerPlay() {
+  let gameOptions = ["rock", "paper", "scissors"];
+  return gameOptions[Math.floor(Math.random() * gameOptions.length)];
+}
 
-let youScore = document.getElementById('your-score-box');
-let compScore = document.getElementById('computer-score-box');
+// Function that will play a new game (pop up button, resets game)
+function playNewGame() {
+    resultsContainer.appendChild(newGame);
 
-const optionButtons = Array.from(document.querySelectorAll('.option-button'));
+}
 
-optionButtons.forEach(btn => btn.addEventListener('click', function() {
-    
-    let computerSelection = computerPlay()
-    let output = playRound(btn.id, computerSelection);
-    
-
-    if (Number(youScore.textContent === 5 || Number(compScore.textContent) === 5)) {
-        if (Number(youScore.textContent) > Number(compScore.textContent)) {
-            youScore.textContent = 0;
-            compScore.textContent = 0;
-            alert('you win!');     
-        } else if (Number(compScore.textContent) > (Number(youScore.textContent))) {
-            youScore.textContent = 0;
-            compScore.textContent = 0;
-            alert('you lost!')
-        } 
-    } else { 
-        if (output.indexOf('You Win') === 0) {
-            resultsOutput.textContent = output;
-            youScore.textContent = Number(youScore.textContent) + 1; 
-        } else if (output.indexOf('You Lose') === 0) {
-            resultsOutput.textContent = output;
-            compScore.textContent = Number(compScore.textContent) + 1; 
-        } else {
-            resultsOutput.textContent = output;
-            
-        }
-    }
-    
-}))
+// Function that refreshes page
+function refreshPage() {
+    window.location.reload();
+}
 
 
+// Determines ifthe game is over
+function isGameOver() {
+  if (playerScore >= 5 || computerScore >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Capitalizes a string
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+// Event Listeners
+optionButtons.forEach((btn) => btn.addEventListener("click", playGame));
+newGame.addEventListener('click', refreshPage)
